b678d213c3f43a28e654edbd180e9fb08c9a046e
narendrajava1 <30337207+narendrajava1@users.noreply.github.com>
2019-10-25 12:59:35 +0530

    Update Note.java

--- src/main/java/com/easynotes/model/Note.java
+++ src/main/java/com/easynotes/model/Note.java
@@ -2,20 +2,19 @@
  *
  */
 package com.easynotes.model;
-import java.time.Instant;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -23,16 +22,15 @@ import lombok.Data;
 
 @Entity
 @Table(name = "notes")
-@EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
 allowGetters = true)
 @Data
 public class Note {
 	@NotBlank
 	private String content;
-	@CreatedDate
+	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)", insertable = false, updatable = false)
-	private Instant createdAt;
+	private Date createdAt;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,7 +39,8 @@ public class Note {
 	@NotBlank
 	private String title;
 	@LastModifiedDate
-	@Column(nullable = false, columnDefinition = "DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)", insertable = false, updatable = false)
-	private Instant updatedAt;
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)", insertable = false, updatable = true)
+	private Date updatedAt;
 
 }
